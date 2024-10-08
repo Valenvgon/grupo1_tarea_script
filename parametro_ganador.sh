42847b06d50e34f1821a942e155c38a32153b9ca
Valenvgon <vvercherg@gmail.com>
2024-10-08 11:08:26 +0200

    update and finish tarea script
    he añadido la parte de los minutos

--- parametro_ganador.sh
+++ parametro_ganador.sh
@@ -2,3 +2,12 @@
 mkdir  $1
 cd $1
 touch  ganador.txt
+
+minuto=$(date +%M)
+
+# Verificar si el minuto es par o impar
+if (( minuto % 2 == 0 )); then
+  echo "Eres ganador!"
+else
+  echo "Eres perdedor. Han pasado $minuto minutos."
+fi
